09acda40b2d746358ab5e2e0cfbedb6488dff28d
yashk0thari <yashkothari1000@g.ucla.edu>
2023-04-22 19:10:59 -0700

    Added Audio + txt endpoints

--- Backend/main.py
+++ Backend/main.py
@@ -4,8 +4,10 @@ from pre_process import data_preprocess
 from structure import Graph, Node
 
 #import Relevant libraries
-from flask import Flask, request, make_response, redirect, url_for
+from flask import Flask, request, make_response, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
+from whisp import run_whisper
 from fastapi import FastAPI, HTTPException
 import requests
 import os
@@ -14,6 +16,15 @@ graph = Graph()
 
 # Define App:
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/Users/valley/Desktop/External_Finder/Hackathons/la_hacks_2023/Backend/uploads'  # Choose the folder where you want to save the uploaded files.
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Set the maximum allowed file size to 16 MB.
+app.config['SECRET_KEY'] = 'your_secret_key'  # Set your secret key for flash messages.
+
+ALLOWED_EXTENSIONS = {'txt', 'wav'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 CORS(app, origins=['http://localhost:3000'])
 
 ### Define Routes:
@@ -44,21 +55,77 @@ def process_graph_object(text):
         create_new_node(sentence, graph)
     return graph.to_json()
 
-@app.route('/post/initial_file', methods = ['POST'])
-def init_file():
+@app.route('/user_input/audio/', methods = ['POST'])
+def process_user_audio_input():
+    if request.method == 'POST':
+        if 'fileName' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        file = request.files['fileName']
+        if file.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            if (".wav" in filename):
+                text = run_whisper("/Users/valley/Desktop/External_Finder/Hackathons/la_hacks_2023/Backend/uploads/" + filename)
 
-    audio_file = request.files['audio']
+                payload = {"text": text}
 
-    # TODO: Add transcription code + Inital File Handling 
+                requests.get(f"http://127.0.0.1:8080/graph_object_from_file/", params = payload)
 
-    return 'Received (POST) init_file request!'
+            return jsonify({"success": "File uploaded successfully."}), 200
+    return '''
+    <!doctype html>
+    <title>Upload File</title>
+    <h1>Upload File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
+@app.route('/user_input/text/', methods = ['POST'])
+def process_user_text_input():
+    if request.method == 'POST':
+        if 'fileName' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        file = request.files['fileName']
+        if file.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            if (".txt" in filename):
+                with open("/Users/valley/Desktop/External_Finder/Hackathons/la_hacks_2023/Backend/uploads/" + filename) as f:
+                    text = ""
+                    for i in f.readlines():
+                        text += i + ". "
 
-@app.route('/post/graph/<string:graphID>')
-def postGraph(graphID):
-    
-    # TODO: Generate Graph + Nodes
-    return
+                    payload = {"text": text}
+                    requests.get(f"http://127.0.0.1:8080/graph_object_from_file/", params = payload)
+                
+
+            return jsonify({"success": "File uploaded successfully."}), 200
+    return '''
+    <!doctype html>
+    <title>Upload File</title>
+    <h1>Upload File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route("/graph_object_from_file/", methods = ['GET'])
+def process_graph_object_from_file():
+    text = request.args.get("text")
+    text_sentences = data_preprocess(text)
+    print(">> ", text_sentences)
+    for sentence in text_sentences:
+        if (sentence == ""):
+            raise HTTPException(status_code=404, detail="Sentence cannot be null.")
+        create_new_node(sentence, graph)
+    return graph.to_json()
 
 
 
