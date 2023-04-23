0b43ff139908c2b70722d60108f369a9a68939ab
Arjun Loomba <34513460+arjunloomba1311@users.noreply.github.com>
2023-04-22 18:06:35 -0700

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -2,11 +2,10 @@
 from model import create_new_node
 from pre_process import data_preprocess
 from structure import Graph, Node
-from werkzeug.utils import secure_filename
-from whisp import run_whisper
 
 #import Relevant libraries
-from flask import Flask, request, flash, redirect, url_for, jsonify
+from flask import Flask, request, make_response, redirect, url_for
+from flask_cors import CORS
 from fastapi import FastAPI, HTTPException
 import requests
 import os
@@ -15,15 +14,7 @@ graph = Graph()
 
 # Define App:
 app = Flask(__name__)
-
-app.config['UPLOAD_FOLDER'] = '/Users/valley/Desktop/External_Finder/Hackathons/la_hacks_2023/Backend/uploads'  # Choose the folder where you want to save the uploaded files.
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Set the maximum allowed file size to 16 MB.
-app.config['SECRET_KEY'] = 'your_secret_key'  # Set your secret key for flash messages.
-
-ALLOWED_EXTENSIONS = {'txt', 'wav'}
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+CORS(app, origins=['http://localhost:3000'])
 
 ### Define Routes:
 # Home-Page API:
@@ -33,66 +24,19 @@ def home():
 
 @app.route("/user_input", methods = ['POST'])
 def process_user_input():
-    # if request.method == 'POST':
-    #     if 'file' not in request.files:
-    #         flash('No file part')
-    #         return redirect(request.url)
-    #     file = request.files['file']
-    #     if file.filename == '':
-    #         flash('No selected file')
-    #         return redirect(request.url)
-    #     if file and allowed_file(file.filename):
-    #         filename = secure_filename(file.filename)
-    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    #         flash('File uploaded successfully.')
-    #         return redirect(url_for('upload_file'))
-    # return '''
-    # <!doctype html>
-    # <title>Upload File</title>
-    # <h1>Upload File</h1>
-    # <form method=post enctype=multipart/form-data>
-    #   <input type=file name=file>
-    #   <input type=submit value=Upload>
-    # </form>
-    # '''
-    if request.method == 'POST':
-        if 'fileName' not in request.files:
-            return jsonify({"error": "No file part"}), 400
-        file = request.files['fileName']
-        if file.filename == '':
-            return jsonify({"error": "No selected file"}), 400
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            if (".wav" in filename):
-                text = run_whisper("/Users/valley/Desktop/External_Finder/Hackathons/la_hacks_2023/Backend/uploads/" + filename)
+    if request.method == "POST":
+        user_text = request.json['text']
 
-                payload = {"text": text}
 
-                response = requests.get(f"http://127.0.0.1:8080/graph_object/", params = payload)
-            if (".txt" in filename):
-                with open("/Users/valley/Desktop/External_Finder/Hackathons/la_hacks_2023/Backend/uploads/" + filename) as f:
-                    text = ""
-                    for i in f.readlines():
-                        text += i + ". "
+        # Generate the URL for the target endpoint
+        response = requests.get(f"http://127.0.0.1:8080/graph_object/{user_text}")
 
-                    payload = {"text": text}
-                    response = requests.get(f"http://127.0.0.1:8080/graph_object/", params = payload)
+        # # Make an HTTP request to the target endpoint
+        # response = requests.get(endpoint2_url)
+    return response.content
 
-            return jsonify({"success": "File uploaded successfully."}), 200
-    return '''
-    <!doctype html>
-    <title>Upload File</title>
-    <h1>Upload File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-@app.route("/graph_object/", methods = ['GET'])
-def process_graph_object():
-    text = request.args.get("text")
+@app.route("/graph_object/<string:text>", methods = ['GET'])
+def process_graph_object(text):
     text_sentences = data_preprocess(text)
     for sentence in text_sentences:
         if (sentence == ""):
@@ -116,5 +60,9 @@ def postGraph(graphID):
     # TODO: Generate Graph + Nodes
     return
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
